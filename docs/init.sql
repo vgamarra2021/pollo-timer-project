0e81e9bb53de706396f17616edb2695dba78a691
Victor Hugo Gamarra Villanueva <victorgamarra1@gmail.com>
2026-08-04 13:10:28 -0500

    feat: Add init.sql

---
+++ docs/init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS "action" (
+	"action_id" INTEGER NOT NULL,
+	"created_at" TIMESTAMP,
+	"type" VARCHAR,
+	"session_id" INTEGER NOT NULL,
+	PRIMARY KEY("action_id")
+);
+
+CREATE TABLE IF NOT EXISTS "session" (
+	"session_id" INTEGER NOT NULL,
+	"seconds_duration" NUMERIC,
+	"started_at" TIMESTAMP,
+	"finish_at" TIMESTAMP,
+	PRIMARY KEY("session_id"),
+	FOREIGN KEY ("session_id") REFERENCES "action"("session_id")
+	ON UPDATE NO ACTION ON DELETE NO ACTION
+);
